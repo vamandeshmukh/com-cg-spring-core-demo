fbde63cdff526c7afbe0493e855875207c7f290e
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-10 10:43:24 +0530

    Co
    Co

--- src/main/java/com/cg/spring/core/demo/App.java
+++ src/main/java/com/cg/spring/core/demo/App.java
@@ -1,13 +1,15 @@
 package com.cg.spring.core.demo;
 
 /**
- * Hello world!
+ * 
+ * @author Vaman Deshmukh Spring core demo project for CG trainees
+ * 
+ *         12 Oct 2021 batch 3
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! 2" );
-    }
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! 2");
+	}
 }
