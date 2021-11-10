31279e64cedcffbf84614bf330ec49bae16a3db2
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-10 11:48:31 +0530

    Update App.java

--- src/main/java/com/cg/spring/core/demo/App.java
+++ src/main/java/com/cg/spring/core/demo/App.java
@@ -1,5 +1,8 @@
 package com.cg.spring.core.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.cg.spring.core.demo.model.Employee;
 
 /**
@@ -9,10 +12,17 @@ import com.cg.spring.core.demo.model.Employee;
  */
 
 public class App {
+
 	public static void main(String[] args) {
+
 		System.out.println("Start");
 
-		Employee emp = new Employee(101, "Sonu", 10.6);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext();
+
+//		Employee emp = new Employee(101, "Sonu", 10.6);
+
+		Employee emp = ctx.getBean(Employee.class);
+
 		System.out.println(emp.toString());
 
 		System.out.println("End");
